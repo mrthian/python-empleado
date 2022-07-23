527af369cd6c475e6f96584f246926d35fd026f4
cristian-giraldo <cristian@simplenight.com>
2022-07-23 01:51:54 -0500

    feat: added setting to prod

--- project01/settings/prod.py
+++ project01/settings/prod.py
@@ -3,7 +3,12 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '147.182.217.171',
+    "127.0.0.3",
+    "127.0.0.1",
+    "localhost",
+]
 
 
 # Database
